81b72ddc9a57d6eaf96e795c808222e7bda95fe0
dukeweezo <dukeweezo@gmail.com>
2022-06-23 16:16:38 +0000

    Work on second lab

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,6 +20,9 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('registration/', views.registration_request, name='registration'),
     path(route='about', view=views.about, name='about'),
     path(route='contact', view=views.contact, name='contact'),
 
